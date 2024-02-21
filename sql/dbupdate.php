4f327f8cce030e34ab38af2db94c13c484629f54
Daniel Cazalla (ZallaxDev) <86362063+ZallaxDev@users.noreply.github.com>
2024-02-21 09:32:59 +0100

    Table creation for configuration and build update

--- sql/dbupdate.php
+++ sql/dbupdate.php
@@ -1,6 +1,5 @@
+
 <#1>
-<?php ?>
-<#2>
 <?php
 GLOBAL $DIC;
 $ilDB = $DIC->database();
@@ -26,5 +25,33 @@ if(!$ilDB->tableExists("rep_robj_xswb_data")) {
     $ilDB->createTable("rep_robj_xswb_data", $fields);
     $ilDB->addPrimaryKey("rep_robj_xswb_data", array("id"));
 }
+
+?>
+
+<#2>
+<?php
+    GLOBAL $DIC;
+    $ilDB = $DIC->database();
+    $fields = array(
+        'id' => array(
+            'type' => 'integer',
+            'length' => 4,
+            'notnull' => true
+        ),
+        'config_key' => array(
+            'type' => 'text',
+            'length' => 255,
+            'notnull' => true
+        ),
+        'value' => array(
+            'type' => 'text',
+            'length' => 255,
+            'notnull' => false
+        )
+    );
+    if(!$ilDB->tableExists("xswb_config")) {
+        $ilDB->createTable("xswb_config", $fields);
+        $ilDB->addPrimaryKey("xswb_config", array("id"));
+    }
 
 ?>
