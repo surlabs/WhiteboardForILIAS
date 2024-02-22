7cc0f15ff19c32747989b5f6315b9896d23dea2d
jcopado <jcopado@surlabs.es>
2024-02-22 18:49:20 +0100

    Version 1.1.1

--- plugin.php
+++ plugin.php
@@ -1,19 +1,29 @@
 <?php
+/*
+ *  This file is part of the Whiteboard Repository Object plugin for ILIAS, a collaborative online whiteboard tool,
+ *  developed by SURLABS with funding from the University of Freiburg.
+ *
+ *  This plugin is freely distributed under the terms of the GNU General Public License version 3 (GPL-3.0),
+ *  a copy of which is available at https://www.gnu.org/licenses/gpl-3.0.en.html. This license allows for the free use,
+ *  modification, and distribution of this software, ensuring it remains open-source and accessible to the community.
+ *
+ *  The Whiteboard plugin uses a version the tldraw library, which is also open-source and distributed under its specific
+ *  terms and conditions. For details on the tldraw license, please refer to https://github.com/tldraw/tldraw/blob/main/LICENSE.md.
+ *
+ *  DISCLAIMER: The developers, contributors, and funding entities associated with the Whiteboard plugin or the tldraw library
+ *  assume no responsibility for any damages or losses incurred from the use of this software. Users are encouraged to review
+ *  the license agreements and comply with the terms and conditions set forth.
+ *
+ *  Community involvement is welcome. To report bugs, suggest improvements, or participate in discussions,
+ *  please visit the Mantis system and search for ILIAS Plugins under the "Whiteboard" category at https://mantis.ilias.de.
+ *
+ *  For further information, documentation, and the source code, visit our GitHub repository at
+ *  https://github.com/surlabs/Whiteboard.
+ */
 $id = "xswb";
- 
-// code version; must be changed for all code changes
 $version = "1.1.1";
-
-// ilias min and max version; must always reflect the versions that should
-// run with the plugin
 $ilias_min_version = "7.0";
 $ilias_max_version = "8.999";
-   
-// optional, but useful: Add one or more responsible persons and a contact email
 $responsible = "Jesús Copado";
 $responsible_mail = "jcopado@surlabs.es";
-
 $supports_export = true;
-
-// Enable learning progress
-$learning_progress = true;
