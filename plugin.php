7cbafcceaf86adefc2ec863119cf5e19185048b6
Daniel Cazalla (ZallaxDev) <86362063+ZallaxDev@users.noreply.github.com>
2024-09-19 13:40:21 +0200

    Content embedding options removed. Updated plugin version.

--- plugin.php
+++ plugin.php
@@ -21,7 +21,7 @@
  *  https://github.com/surlabs/Whiteboard.
  */
 $id = "xswb";
-$version = "1.1.2";
+$version = "7.1.2";
 $ilias_min_version = "7.0";
 $ilias_max_version = "8.999";
 $responsible = "Jesús Copado";
