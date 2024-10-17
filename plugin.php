b5b4cf822456e10b65356a562e0b564d9ed3faaa
Daniel Cazalla (ZallaxDev) <86362063+ZallaxDev@users.noreply.github.com>
2024-10-17 15:12:22 +0200

    Update plugin version to 9.0.2

--- plugin.php
+++ plugin.php
@@ -23,7 +23,7 @@
 $id = "xswb";
  
 // code version; must be changed for all code changes
-$version = "9.0.1";
+$version = "9.0.2";
 
 // ilias min and max version; must always reflect the versions that should
 // run with the plugin
