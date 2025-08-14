78ba5f16f5fc701adf1fdc8b59b5362556352e9c
Daniel Cazalla <86362063+ZallaxDev@users.noreply.github.com>
2025-08-14 10:24:01 +0200

    Update plugin.php

--- plugin.php
+++ plugin.php
@@ -23,12 +23,13 @@
 $id = "xswb";
  
 // code version; must be changed for all code changes
-$version = "8.1";
+$version = "10.0";
 
 // ilias min and max version; must always reflect the versions that should
 // run with the plugin
-$ilias_min_version = "6.0";
+$ilias_min_version = "10.0";
 $ilias_max_version = "10.999";
 $responsible = "Jesús Copado";
 $responsible_mail = "jcopado@surlabs.com";
 $supports_export = true;
+
