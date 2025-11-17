7a2d3aaa1a7e904c6f03d4d3caa42e4ae7afe2c6
Daniel Cazalla (ZallaxDev) <86362063+ZallaxDev@users.noreply.github.com>
2025-11-17 14:22:16 +0100

    fix: fix template path

--- classes/class.ilObjWhiteboardGUI.php
+++ classes/class.ilObjWhiteboardGUI.php
@@ -188,7 +188,7 @@ class ilObjWhiteboardGUI extends ilObjectPluginGUI
         $object = $this->object;
         $tpl->addJavaScript('Customizing/global/plugins/Services/Repository/RepositoryObject/Whiteboard/render/templates/default/index.js');
 
-        $board = new ilTemplate('index.html', true, true, "Customizing/global/plugins/Services/Repository/RepositoryObject/Whiteboard/render");
+        $board = new ilTemplate('index.html', true, true, "public/Customizing/global/plugins/Services/Repository/RepositoryObject/Whiteboard/render");
 
         $idIlias = $this->getObject()->getId();
         $userName = $DIC->user()->getFullname();
@@ -218,8 +218,8 @@ class ilObjWhiteboardGUI extends ilObjectPluginGUI
     {
         return ($this->checkPermissionBool("redact") || $this->checkPermissionBool("write"));
     }
-
-    protected function checkImportExportPermission(): bool
+	
+	protected function checkImportExportPermission(): bool
     {
         global $DIC;
         return $DIC->access()->checkAccess("write", "", $this->object->getRefId());
