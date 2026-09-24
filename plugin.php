bf2fb45b0a61b626d58f0630cc1d16396ed8cd37
Saúl Díaz <sauldiazdiaz210@gmail.com>
2026-09-24 13:55:14 +0200

    Version 10.3.1

--- plugin.php
+++ plugin.php
@@ -23,13 +23,13 @@
 $id = "xswb";
  
 // code version; must be changed for all code changes
-$version = "10.3";
+$version = "10.3.1";
 
 // ilias min and max version; must always reflect the versions that should
 // run with the plugin
 $ilias_min_version = "10.0";
 $ilias_max_version = "10.999";
-$responsible = "Jesús Copado";
-$responsible_mail = "jcopado@surlabs.com";
+$responsible = 'Jesus Copado';
+$responsible_mail = 'jcopado@surlabs.com';
 $supports_export = true;
 
